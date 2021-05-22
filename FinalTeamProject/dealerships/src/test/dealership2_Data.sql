b1d7b3723e840dc16e435840cc01f83ec6a451fb
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-05-22 15:09:58 -0400

    Update dealership2_Data.sql
    minor corrections

--- FinalTeamProject/dealerships/src/test/dealership2_Data.sql
+++ FinalTeamProject/dealerships/src/test/dealership2_Data.sql
@@ -1,8 +1,4 @@
-create DATABASE if not exists dealership;
-use dealership;
-drop table if exists transactions;
-drop table if exists vehicles;
-drop table if exists customers;
+
 -- customers DATA, 5 records minimum
 -- vehicles DATA, 4 records minimum
 -- transactions DATA, 3 records minimum
