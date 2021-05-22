b83700386951a34b9fe69665b9167a42b5abc47e
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-05-22 15:11:00 -0400

    Update dealership2_Schema.sql
    minor edits

--- FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
+++ FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
@@ -10,14 +10,14 @@ create table dealerships ();
 create table vehicles ();
 create table employees ();
 create table transactions (
-  transaction_ID int (10 NOT NULL AUTO_INCREMENT),
-  vehicle_ID int (10) NOT NULL,
-  customer_ID int(10) NOT NULL,
-  employee_ID int(10) NOT NULL,
-  dealership_ID int(10) NOT NULL,
+  transactionID int (10 NOT NULL AUTO_INCREMENT),
+  vehicleID int (10) NOT NULL,
+  customerID int(10) NOT NULL,
+  employeeID int(10) NOT NULL,
+  dealershipID int(10) NOT NULL,
   datestamp date NOT NULL,
-  PRIMARY KEY (transaction_ID),
-  FOREIGN KEY (vehicle_ID) REFERENCES vehicles(vehicle_ID),
-  FOREIGN KEY (customer_ID) REFERENCES customers(customer_ID),
-  FOREIGN KEY (employee_ID) REFERENCES employees(employee_ID),
-  FOREIGN KEY (dealership_ID) REFERENCES dealerships(dealership_ID)
+  PRIMARY KEY (transactionID),
+  FOREIGN KEY (vehicleID) REFERENCES vehicles(vehicleID),
+  FOREIGN KEY (customerID) REFERENCES customers(customerID),
+  FOREIGN KEY (employeeID) REFERENCES employees(employeeID),
+  FOREIGN KEY (dealershipID) REFERENCES dealerships(dealershipID)
