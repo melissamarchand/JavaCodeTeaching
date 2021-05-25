1508a2ae5bcc2df11379b22c1f070b36746a6c45
becktrb <78628885+becktrb@users.noreply.github.com>
2021-05-25 19:54:00 -0400

    Update dealership2_Schema.sql
    adding vehicles and customers tables

--- FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
+++ FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
@@ -5,9 +5,24 @@ drop table if exists employees;
 drop table if exists vehicles;
 drop table if exists dealerships;
 drop table if exists customers;
-create table customers ();
+CREATE TABLE customers (
+  customerID int (10) NOT NULL AUTO_INCREMENT,
+  name varchar(45),
+  address varchar(45),
+  phone varchar(20),
+  PRIMARY KEY (customerID)
+);
 create table dealerships ();
-create table vehicles ();
+CREATE TABLE vehicles (
+  vehicleID int (10) NOT NULL AUTO_INCREMENT,
+  dealershipID int (10) NOT NULL AUTO_INCREMENT,
+  is_sold boolean NOT NULL,
+  price decimal(9, 2) NOT NULL,
+  model varchar(40) NOT NULL,
+  make varchar(40) NOT NULL,
+  PRIMARY KEY (vehicleID),
+  FOREIGN KEY (dealershipID) REFERENCES dealerships(dealershipID)
+);
 create table employees ();
 create table transactions (
   transactionID int (10 NOT NULL AUTO_INCREMENT),
