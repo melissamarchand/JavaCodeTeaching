a04ff4d34cce9028d39a36f50d66ec4b6d74f3b0
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-05-27 13:13:32 -0400

    Update dealership2_Data.sql
    update comment for new table name

--- FinalTeamProject/dealerships/src/test/dealership2_Data.sql
+++ FinalTeamProject/dealerships/src/test/dealership2_Data.sql
@@ -1,6 +1,5 @@
-
 -- customers DATA, 5 records minimum
 -- vehicles DATA, 4 records minimum
 -- transactions DATA, 3 records minimum
--- dealerships DATA, 2 records minimum
+-- locations DATA, 2 records minimum
 -- employees DATA, 3 records minimum
