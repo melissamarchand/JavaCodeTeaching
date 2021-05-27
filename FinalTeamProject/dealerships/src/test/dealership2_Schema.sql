242718a170875297f4fd45f812a37cb9429a2dcc
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-05-27 10:34:45 -0400

    Update dealership2_Schema.sql
    changed dealerships table and all related fields to locations, fixed typo errors.

--- FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
+++ FinalTeamProject/dealerships/src/test/dealership2_Schema.sql
@@ -3,7 +3,7 @@ use dealership;
 drop table if exists transactions;
 drop table if exists employees;
 drop table if exists vehicles;
-drop table if exists dealerships;
+drop table if exists locations;
 drop table if exists customers;
 CREATE TABLE customers (
   customerID int (10) NOT NULL AUTO_INCREMENT,
@@ -15,24 +15,25 @@ CREATE TABLE customers (
 create table dealerships ();
 CREATE TABLE vehicles (
   vehicleID int (10) NOT NULL AUTO_INCREMENT,
-  dealershipID int (10) NOT NULL AUTO_INCREMENT,
+  locationID int (10) NOT NULL AUTO_INCREMENT,
   is_sold boolean NOT NULL,
   price decimal(9, 2) NOT NULL,
   model varchar(40) NOT NULL,
   make varchar(40) NOT NULL,
   PRIMARY KEY (vehicleID),
-  FOREIGN KEY (dealershipID) REFERENCES dealerships(dealershipID)
+  FOREIGN KEY (locationID) REFERENCES locations(locationID)
 );
 create table employees ();
 create table transactions (
-  transactionID int (10 NOT NULL AUTO_INCREMENT),
+  transactionID int (10) NOT NULL AUTO_INCREMENT,
   vehicleID int (10) NOT NULL,
   customerID int(10) NOT NULL,
   employeeID int(10) NOT NULL,
-  dealershipID int(10) NOT NULL,
+  locationID int(10) NOT NULL,
   datestamp date NOT NULL,
   PRIMARY KEY (transactionID),
   FOREIGN KEY (vehicleID) REFERENCES vehicles(vehicleID),
   FOREIGN KEY (customerID) REFERENCES customers(customerID),
   FOREIGN KEY (employeeID) REFERENCES employees(employeeID),
-  FOREIGN KEY (dealershipID) REFERENCES dealerships(dealershipID)
+  FOREIGN KEY (locationID) REFERENCES locations(locationID)
+);
